b8e5051a80f602890a421568af3582feb6caa697
Lucas Delobelle <lucas@ld-web.net>
2023-09-21 15:29:53 +0200

    :sparkles: add check Controller

--- src/Controller/SpamController.php
+++ src/Controller/SpamController.php
@@ -2,17 +2,39 @@
 
 namespace App\Controller;
 
+use Egulias\EmailValidator\EmailValidator;
+use Egulias\EmailValidator\Validation\RFCValidation;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class SpamController extends AbstractController
 {
-  #[Route('/api/check', name: 'app_spam_check')]
-  public function index(): Response
+  private const SPAM_DOMAINS = ["test.com", "mailinator.com", "youpi.net"];
+
+  #[Route('/api/check', name: 'app_spam_check', methods: "POST")]
+  public function check(Request $request): JsonResponse
   {
-    return $this->render('spam/index.html.twig', [
-      'controller_name' => 'SpamController',
-    ]);
+    $data = $request->toArray();
+    $email = $data['email'];
+
+    $validator = new EmailValidator();
+
+    if (!$validator->isValid($email, new RFCValidation())) {
+      return $this->json(
+        ["error" => "Email au format incorrect"],
+        Response::HTTP_BAD_REQUEST
+      );
+    }
+
+    $emailParts = explode("@", $email);
+
+    if (in_array($emailParts[1], self::SPAM_DOMAINS)) {
+      return $this->json(['result' => 'spam']);
+    }
+
+    return $this->json(['result' => 'ok']);
   }
 }
